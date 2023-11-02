868c5a43ce9fc84e01f23270e7d94805428881f3
cjstepan <calvinstepan@gmail.com>
2023-11-02 15:19:04 -0500

    Initial Project Commit This commit initializes every part of assignment 4 that we should need. Everything moving forward should be adding functionality to the project. - Added KarateSchool.mdf in case the database has problems moving between people - Created KarateSchool Project - Created Administrator.aspx - Created Member.aspx - Created Instructor.aspx - Created KarateSchool.dbml (for LINQ queries) - Created 10 Member records in Member and NetUser tables - Created 5 Instructor records in Instructor and NetUser tables - Created 10 Section records in Section table - Added login module to KarateSchool.master to facilitate login - Added header and footer to KarateSchool.master to facilitate proper page navigation.

---
+++ KarateSchool/Member.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/KarateSchool.Master" AutoEventWireup="true" CodeBehind="Member.aspx.cs" Inherits="KarateSchool.Member" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <p>
+        <br />
+    </p>
+    <p>
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+    </p>
+    <p>
+    </p>
+</asp:Content>
---
+++ KarateSchool/Member.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace KarateSchool
+{
+    public partial class Member : System.Web.UI.Page
+    {
+        KarateSchoolDataContext dbcon;
+        string connString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=D:\\Github\\KarateSchool\\KarateSchool\\App_Data\\KarateSchool.mdf;Integrated Security=True;Connect Timeout=30";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            dbcon = new KarateSchoolDataContext(connString);
+        }
+    }
+}
